e7363d6470a914acb88ef71671a363dfe9f99183
Unknown <fivehoho75@gmail.com>
2019-03-08 13:15:51 +0900

    todoList with typescript is done!

---
+++ src/store/configureStore.ts
@@ -0,0 +1,12 @@
+import modules, { StoreState } from './modules';
+import { createStore, Store } from 'redux';
+
+export default function confiqureStore(): Store<StoreState> {
+    const store = createStore(
+        modules,
+        (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+            (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+    return store;
+}
